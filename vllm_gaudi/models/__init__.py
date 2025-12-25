62b000a3dc8f23896a6135c16ed6110ac906132c
Ma Jian <jian1.ma@intel.com>
2025-12-25 15:18:10 +0800

    move patch into plugin

--- vllm_gaudi/models/__init__.py
+++ vllm_gaudi/models/__init__.py
@@ -3,6 +3,7 @@ from vllm.model_executor.models.registry import ModelRegistry
 
 def register_model():
     from vllm_gaudi.models.gemma3_mm import HpuGemma3ForConditionalGeneration  # noqa: F401
+    import vllm_gaudi.models.deepseek_v32 # noqa: F401
 
     ModelRegistry.register_model(
         "Gemma3ForConditionalGeneration",  # Original architecture identifier in vLLM
